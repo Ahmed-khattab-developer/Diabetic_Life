a23fba663f7447e27d7c11b8e3c93e2dc11187b6
engahmedkhattab <46701046+engahmedkhattab@users.noreply.github.com>
2022-05-30 18:15:24 +0200

    2222

---
+++ app/src/main/java/com/rx/diabeticlife/service/NotificationService.kt
@@ -0,0 +1,90 @@
+package com.rx.diabeticlife.service
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.graphics.Color
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.rx.diabeticlife.R
+import com.rx.diabeticlife.SessionManagement
+import com.rx.diabeticlife.SplashActivity
+import com.rx.diabeticlife.doctor.activity.MainDoctorActivity
+import com.rx.diabeticlife.patient.activity.MainPatientActivity
+import com.rx.diabeticlife.trainer.activity.MainTrainerActivity
+
+class NotificationService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val channelId = "diet"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+
+            val audioAttributes = AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .build()
+
+            val name: CharSequence = "diet"
+            val description = "diet"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val mChannel = NotificationChannel(channelId, name, importance)
+            mChannel.description = description
+            mChannel.enableLights(true)
+            mChannel.lightColor = Color.RED
+            mChannel.setSound(defaultSoundUri, audioAttributes)
+            mChannel.enableVibration(true)
+            mChannel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+            mChannel.setShowBadge(false)
+            notificationManager.createNotificationChannel(mChannel)
+        }
+
+        val intent: Intent = when (SessionManagement.Constants.TYPE) {
+            "doctor" -> {
+                Intent(this, MainDoctorActivity::class.java)
+            }
+            "patient" -> {
+                Intent(this, MainPatientActivity::class.java)
+            }
+            "trainer" -> {
+                Intent(this, MainTrainerActivity::class.java)
+            }
+            else -> {
+                Intent(this, SplashActivity::class.java)
+            }
+        }
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.logo)
+            .setContentTitle(remoteMessage.notification?.title)
+            .setContentText(remoteMessage.notification?.body)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setStyle(
+                NotificationCompat.BigTextStyle().bigText(remoteMessage.notification?.body)
+            )
+            .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        notificationManager.notify(0, builder.build())
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+
+}
